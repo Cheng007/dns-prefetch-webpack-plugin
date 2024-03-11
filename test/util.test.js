9e15b237e698b32333591c75a2fc2df0dd930d6d
Cheng007 <565460621@qq.com>
2024-03-11 17:36:13 +0800

    test: util test

---
+++ test/util.test.js
@@ -0,0 +1,80 @@
+const { expect, describe, it } = require("@jest/globals");
+const { getAllDomain, getPrefetchHtmlString } = require("../lib/util");
+
+describe("util", () => {
+  const html = `<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <title>Webpack App</title>
+  <link href="http://example.com/path.css" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <script defer src="main.bundle.js"></script>
+  <script src="https://zhongan.com/path/test.js"></script>
+</head>
+<body>
+  hell world
+</body>
+</html>`;
+
+  it("get all domain", () => {
+    const domains = getAllDomain(html);
+
+    expect(domains).toHaveLength(2);
+    expect(domains).toEqual(
+      expect.arrayContaining(["example.com", "zhongan.com"]),
+    );
+  });
+
+  describe("dns-prefetch", () => {
+    it("with custom domains", () => {
+      const customDomains = ["a.com", "b.com"];
+      const dnsPrefetchHtml = getPrefetchHtmlString(html, customDomains);
+
+      const targetHtml = `<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <title>Webpack App</title>
+  <link href="http://example.com/path.css" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <script defer src="main.bundle.js"></script>
+  <script src="https://zhongan.com/path/test.js"></script>
+
+<link rel="dns-prefetch" href="//a.com" />
+<link rel="dns-prefetch" href="//b.com" />
+
+</head>
+<body>
+  hell world
+</body>
+</html>`;
+
+      expect(dnsPrefetchHtml).toBe(targetHtml);
+    });
+
+    it("without custom domain", () => {
+      const dnsPrefetchHtml = getPrefetchHtmlString(html);
+      const targetHtml = `<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <title>Webpack App</title>
+  <link href="http://example.com/path.css" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <script defer src="main.bundle.js"></script>
+  <script src="https://zhongan.com/path/test.js"></script>
+
+<link rel="dns-prefetch" href="//zhongan.com" />
+<link rel="dns-prefetch" href="//example.com" />
+
+</head>
+<body>
+  hell world
+</body>
+</html>`;
+
+      expect(dnsPrefetchHtml).toBe(targetHtml);
+    });
+  });
+});
